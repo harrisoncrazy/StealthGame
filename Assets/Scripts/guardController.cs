0600de13fa3f0c8c2402ebec93c41ab41dafa092
harrisoncrazy <harrisoncrawford16@yahoo.ca>
2016-11-25 20:18:47 -0500

    Guard finding player intial

--- Assets/Scripts/guardController.cs
+++ Assets/Scripts/guardController.cs
@@ -14,51 +14,40 @@ public class guardController : MonoBehaviour {
 	public float z;
 	private float timer;
 	private Vector3 lookLoc;
+	private Vector3 tempStore;
+
+	public GameObject player;
+	private bool foundPlayer = false;
 
 	// Use this for initialization
 	void Start () {
 		x = Random.Range(xMin, xMax);
 		z = Random.Range(zMin, zMax);
-		lookLoc = new Vector3 (x, -0.487f, z);
+		lookLoc = new Vector3 (x, -0.563f, z);
 		transform.LookAt (lookLoc);
+
+		player = GameObject.Find ("Player");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		/*
-		if (transform.localPosition.x > xMax) {
-			x = Random.Range(-velocityMax, 0.0f);
-			angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angle, 0);
-			timer = 0.0f; 
-		}
-		if (transform.localPosition.x < xMin) {
-			x = Random.Range(0.0f, velocityMax);
-			angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angle, 0); 
-			timer = 0.0f; 
-		}
-		if (transform.localPosition.z > zMax) {
-			z = Random.Range(-velocityMax, 0.0f);
-			angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angle, 0); 
-			timer = 0.0f; 
-		}
-		if (transform.localPosition.z < zMin) {
-			z = Random.Range(0.0f, velocityMax);
-			angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angle, 0);
-			timer = 0.0f; 
-		}
-
-*/
-		if (Mathf.Round(transform.localPosition.x) == Mathf.Round(x) && Mathf.Round(transform.localPosition.z) == Mathf.Round(z)) {
+		if (Mathf.Round(transform.localPosition.x) == Mathf.Round(x) && Mathf.Round(transform.localPosition.z) == Mathf.Round(z) && foundPlayer != true) {
 			x = Random.Range(xMin, xMax);
 			z = Random.Range(zMin, zMax);
-			lookLoc = new Vector3 (x, -0.487f, z);
+			lookLoc = new Vector3 (x, -0.563f, z);
 			transform.LookAt (lookLoc);
 		}
 		transform.LookAt (lookLoc);
 		transform.position += transform.forward * Time.deltaTime * velocityMax;
+
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, 4.0f)) {
+			if (hit.collider.tag == "Player") {
+				tempStore = lookLoc;
+				lookLoc = player.transform.position;
+				velocityMax = 2;
+				foundPlayer = true;
+			}
+		}
 	}
 }
