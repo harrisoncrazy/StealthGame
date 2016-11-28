28d31def86c55e839825275a349c3d6a4e18ac94
harrisoncrazy <harrisoncrawford16@yahoo.ca>
2016-11-28 17:41:08 -0500

    random shit

--- Assets/Scripts/guardController.cs
+++ Assets/Scripts/guardController.cs
@@ -13,7 +13,7 @@ public class guardController : MonoBehaviour {
 	private float x;
 	private float z;
 	private float timer;
-	private Vector3 lookLoc;
+	public Vector3 lookLoc;
 
 	//point to point Values
 	private bool pointGenerated = false;
@@ -58,39 +58,44 @@ public class guardController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (isWandering == true) { //Wanders based on random point generated from zmax/min and xmax/min
-			if (Mathf.Round (transform.localPosition.x) == Mathf.Round (x) && Mathf.Round (transform.localPosition.z) == Mathf.Round (z) && foundPlayer != true) {
-				x = Random.Range (xMin, xMax);
-				z = Random.Range (zMin, zMax);
-				lookLoc = new Vector3 (x, -0.563f, z);
-				transform.LookAt (lookLoc);
+			if (foundPlayer == false) {
+				if (Mathf.Round (transform.localPosition.x) == Mathf.Round (x) && Mathf.Round (transform.localPosition.z) == Mathf.Round (z) && foundPlayer != true) {
+					x = Random.Range (xMin, xMax);
+					z = Random.Range (zMin, zMax);
+					lookLoc = new Vector3 (x, -0.563f, z);
+					transform.LookAt (lookLoc);
+				}
 			}
 			transform.LookAt (lookLoc);
 			transform.position += transform.forward * Time.deltaTime * velocityMax;
 		}
 
 		if (isPointToPoint == true) { //wanders from point a to b and back generated from zmax/min and xmax/min using the min value for the start and max for the end
-			if (isAtEndPoint == false) {
-				if (Mathf.Round (transform.localPosition.x) == Mathf.Round (lookLoc.x) && Mathf.Round (transform.localPosition.z) == Mathf.Round (lookLoc.z) && foundPlayer != true) {
-					isAtEndPoint = true;
-					generatePoint ();
+			if (foundPlayer == false) {
+				if (isAtEndPoint == false) {
+					if (Mathf.Round (transform.localPosition.x) == Mathf.Round (lookLoc.x) && Mathf.Round (transform.localPosition.z) == Mathf.Round (lookLoc.z)) {
+						isAtEndPoint = true;
+						generatePoint ();
+					}
 				}
-			}
-			if (isAtEndPoint == true) {
-				if (Mathf.Round (transform.localPosition.x) == Mathf.Round (lookLoc.x) && Mathf.Round (transform.localPosition.z) == Mathf.Round (lookLoc.z) && foundPlayer != true) {
-					isAtEndPoint = false;
-					generatePoint ();
+				if (isAtEndPoint == true) {
+					if (Mathf.Round (transform.localPosition.x) == Mathf.Round (lookLoc.x) && Mathf.Round (transform.localPosition.z) == Mathf.Round (lookLoc.z)) {
+						isAtEndPoint = false;
+						generatePoint ();
+					}
 				}
 			}
 			transform.LookAt (lookLoc);
 			transform.position += transform.forward * Time.deltaTime * velocityMax;
+			
 		}
 
 		if (isScanning == true) {
-			Timer = Timer + Time.deltaTime;
-			float phase = Mathf.Sin (Timer / Period) + originalAngle;
-			transform.rotation = Quaternion.Euler (new Vector3 (0, phase * Angle, 0));
-
-			if (foundPlayer == true) {
+			if (foundPlayer == false) {
+				Timer = Timer + Time.deltaTime;
+				float phase = Mathf.Sin (Timer / Period) + originalAngle;
+				transform.rotation = Quaternion.Euler (new Vector3 (0, phase * Angle, 0));
+			}else if (foundPlayer == true) {
 				transform.LookAt (lookLoc);
 				transform.position += transform.forward * Time.deltaTime * velocityMax;
 			}
@@ -100,7 +105,7 @@ public class guardController : MonoBehaviour {
 		if (Physics.Raycast (transform.position, transform.forward, out hit, 4.0f)) {
 			if (hit.collider.tag == "Player") {
 				lookLoc = player.transform.position;
-				velocityMax = 2;
+				velocityMax = 5;
 				foundPlayer = true;
 			}
 		}
