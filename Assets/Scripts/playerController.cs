481c3f1a3acdde2b26fb460e732ddd9407352743
harrisoncrazy <harrisoncrawford16@yahoo.ca>
2016-11-27 22:48:54 -0500

    tuning camera and player control

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class playerController : MonoBehaviour {
 
 	public float moveSpeed;
-	private float defaultMoveSpeed = 15;
+	private float defaultMoveSpeed = 20; //Default move speed for slowing down/speeding up with shift if implimented, leaving it for now
 
 	private float maxSpeed = 2.5f;
+
+	public float rotationSpeed;
 
 	private float jumpSpeed;
 	public Rigidbody playerRB;
@@ -22,7 +24,7 @@ public class playerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
+		//transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
 
 		if (playerRB.velocity.magnitude < maxSpeed) { //restricting player speed
 			if (Input.GetKey ("w")) { //Forward
@@ -31,32 +33,40 @@ public class playerController : MonoBehaviour {
 			if (Input.GetKey ("s")) { //Back
 				playerRB.AddForce (-transform.forward * moveSpeed);
 			}
+			/* OLD A AND D MOVEMENT STRAFING
 			if (Input.GetKey ("a")) { //left
 				playerRB.AddForce (-transform.right * moveSpeed);
 			}
 			if (Input.GetKey ("d")) { //right
 				playerRB.AddForce (transform.right * moveSpeed);
-			}
+			}*/
+		}
+		if (Input.GetKey ("a")) { //rotating left
+			transform.Rotate(0, -rotationSpeed, 0);
+		}
+		if (Input.GetKey ("d")) {//rotating right
+			transform.Rotate (0, rotationSpeed, 0);
 		}
 
+		/*
 		//slowing down if shift is pressed
 		if (Input.GetKey (KeyCode.LeftShift)) {
-			moveSpeed = defaultMoveSpeed - 5;
+			moveSpeed = defaultMoveSpeed*3;
 		}
 		//setting speed back to normal
 		if (Input.GetKeyUp (KeyCode.LeftShift)) {
 			moveSpeed = defaultMoveSpeed;
-		}
+		}*/
 
 
-		if (Input.GetKeyDown (KeyCode.Space) && isOnGround == true) {
+		if (Input.GetKeyDown (KeyCode.Space) && isOnGround == true) { //jumping
 			playerRB.AddForce(transform.up * jumpSpeed);
 			isOnGround = false;
 		}
 	}
 
 	void OnCollisionEnter (Collision col) {
-		if (col.gameObject.tag == "ground") {//killing an enemy minion
+		if (col.gameObject.tag == "ground") {//hitting the ground again and reseting jump bool
 			isOnGround = true;
 			moveSpeed = defaultMoveSpeed;
 		}
